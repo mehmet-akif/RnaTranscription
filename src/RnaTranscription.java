a847b3b3d43dd99e053c2ac0309712285edad179
mehmet-akif <makif8992@gmail.com>
2023-03-08 11:47:01 -0500

    first commit

---
+++ src/RnaTranscription.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class RnaTranscription {
+    String transcribe(String dnaStrand) {
+        String result = "";
+        char[] chars = dnaStrand.toCharArray();
+        for (char c : chars) {
+            switch (c) {
+                case 'G':
+                    result += "C";
+                    break;
+                case 'C':
+                    result += "G";
+                    break;
+                case 'T':
+                    result += "A";
+                    break;
+                case 'A':
+                    result += "U";
+                    break;
+                default:
+                    result += "";
+            }
+        }
+        return result;
+    }
+}
+
